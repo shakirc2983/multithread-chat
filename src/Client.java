04f8e9c46c6ae7cc1b904dee73dbfc025162f64e
Shakir <shakirc2983@gmail.com>
2024-12-26 15:23:27 +0000

    user prompt added + formatting

--- src/Client.java
+++ src/Client.java
@@ -1,61 +1,61 @@
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketAddress;
 import java.util.Scanner;
 
 public class Client {
-    private final Socket clientSocket;
-    private final BufferedWriter out;
-    private final BufferedReader in;
+  private final Socket clientSocket;
+  private final BufferedWriter out;
+  private final BufferedReader in;
 
-    public Client(String address, int port) throws IOException {
-
-        try {
-            this.clientSocket = new Socket(address,port);
-            this.out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-            this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+  public Client(String address, int port) throws IOException {
+    try {
+      this.clientSocket = new Socket(address, port);
+      this.out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+      this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public void start() throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        String message;
+  public void start(Scanner scanner) throws IOException {
+    String message;
 
-        sendMessage("Client has joined the Server");
+    sendMessage("Client has joined the Server");
 
-        while (scanner.hasNextLine()) {
-            message = scanner.nextLine();
-            if ("exit".equalsIgnoreCase(message)) {
-                sendMessage("Client is disconnecting...");
-                break;
-            }
-            sendMessage(message);
-        }
-
-        out.close();
+    while (scanner.hasNextLine()) {
+      System.out.print("Message: ");
+      message = scanner.nextLine();
+      if ("exit".equalsIgnoreCase(message)) {
+        sendMessage("Client is disconnecting...");
+        break;
+      }
+      sendMessage(message);
     }
+    scanner.close();
+    out.close();
+  }
 
+  public void stop() throws IOException {
+    clientSocket.close();
+  }
 
-    public void stop() throws IOException {
-        clientSocket.close();
-    }
+  private void sendMessage(String message) throws IOException {
+    out.write(message);
+    out.newLine();
+    out.flush();
+  }
 
-    private void sendMessage(String message) throws IOException {
-        out.write(message);
-        out.newLine();
-        out.flush();
-    }
+  public static String userInput(Scanner scanner) {
+    System.out.println("Enter your username:");
+    String username = scanner.nextLine();
+    return username;
+  }
 
-
-    public static void main(String args[]) throws IOException {
-        Client client = new Client("127.0.0.1",8000);
-        client.start();
-        client.stop();
-
-    }
+  public static void main(String args[]) throws IOException {
+    Client client = new Client("127.0.0.1", 8000);
+    Scanner scanner = new Scanner(System.in);
+    String username = userInput(scanner);
+    client.start(scanner);
+    client.stop();
+  }
 }
--- src/Server.java
+++ src/Server.java
@@ -6,42 +6,42 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+  private final ServerSocket serverSocket;
+  private final Socket clientSocket;
+  private final PrintWriter out;
+  private final BufferedReader in;
 
-    private final ServerSocket serverSocket;
-    private final Socket clientSocket;
-    private final PrintWriter out;
-    private final BufferedReader in;
+  public Server(int port) throws IOException {
+    try {
+      this.serverSocket = new ServerSocket(port);
+      System.out.println("Server has started");
+      this.clientSocket = serverSocket.accept();
+      this.out = new PrintWriter(clientSocket.getOutputStream(), true);
+      this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
-    public Server(int port) throws IOException {
-        try {
-            this.serverSocket = new ServerSocket(port);
-            this.clientSocket = serverSocket.accept();
-            this.out = new PrintWriter(clientSocket.getOutputStream(), true);
-            this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+  public void start() throws IOException {
+    System.out.println("Server socket connected with client socket");
+
+    String message;
+    System.out.println(message = in.readLine());
+    while ((message = in.readLine()) != null) {
+      System.out.println("Client: " + message);
     }
 
-    public void start() throws IOException {
-        System.out.println("Server socket connected with client socket");
+    in.close();
+  }
 
-        String message;
-        System.out.println(message = in.readLine());
-        while ((message = in.readLine()) != null) {
-            System.out.println("Client: " + message);
-        }
+  public void stop() throws IOException {
+    this.serverSocket.close();
+  }
 
-        in.close();
-    }
-
-    public void stop() throws IOException {
-        this.serverSocket.close();
-    }
-
-    public static void main(String args[]) throws IOException {
-        Server server = new Server(8000);
-        server.start();
-        server.stop();
-    }
+  public static void main(String args[]) throws IOException {
+    Server server = new Server(8000);
+    server.start();
+    server.stop();
+  }
 }
